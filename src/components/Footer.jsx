bdc813e92506a5f16a071026549895430715b5cb
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-05-25 08:24:28 -0600

    Footer jsx and styles

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./style/Footer.css"
+
+const Footer = () =>{
+    return(
+        <React.Fragment>
+            <div className="Footer">
+                <p>Example very epic ☺</p>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default Footer;
