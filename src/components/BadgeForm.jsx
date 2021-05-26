a3cc8eba1ccfa26e32225a67becd235633bb036f
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-05-26 15:30:39 -0600

    BadgeForm implemented, working almost correctly, it sends unfilled info

---
+++ src/components/BadgeForm.jsx
@@ -0,0 +1,72 @@
+import React, { Component, Fragment } from "react";
+import "./style/BadgeForm.css";
+
+class BadgeForm extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="BadgeForm m-5">
+            <form onSubmit={this.props.onSubmit}>
+                
+                <div className="form-group mb-2">
+                    <label className="mb-1">Name</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        name="name"
+                        onChange={this.props.onChange}
+                        value={this.props.formValues.name}
+                    />
+                </div>
+
+                <div className="form-group mb-2">
+                    <label className="mb-1">Your face URL here</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        name="profile_picture"
+                        onChange={this.props.onChange}
+                        value={this.props.formValues.profile_picture}
+                    />
+                </div>
+                <div className="form-group mb-2">
+                    <label className="mb-1">Background URL</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        name="header_picture"
+                        onChange={this.props.onChange}
+                        value={this.props.formValues.header_picture}
+                    />
+                </div>
+                <div className="form-group mb-2">
+                    <label className="mb-1">Age</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        name="age"
+                        onChange={this.props.onChange}
+                        value={this.props.formValues.age}
+                    />
+                </div>
+                <div className="form-group mb-2">
+                    <label className="mb-1">City</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        name="city"
+                        onChange={this.props.onChange}
+                        value={this.props.formValues.city}
+                    />
+                    <button type="submit" className="Submit__button">
+                        Yes?
+                    </button>
+                </div>
+            </form>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default BadgeForm;
