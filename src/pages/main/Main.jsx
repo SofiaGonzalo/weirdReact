b6371d06d29fd97b2e3b199d5a5050f388dc8292
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-05-26 21:16:45 -0600

    main changed to new size, added comments

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -7,14 +7,17 @@ const Main = (props) => {
     return(
         <React.Fragment>
             <div className="Main">
+
                 <div className="Main__content">
-                    <h1 className="Main__title">{props.title}</h1>
-                    <Button contentText={"Click 4 fun"}></Button>
+                    <h1 className="Main__title">{props.title || "Welcome"}</h1>
+                    <Button contentText={"Click 4 fun"} Link={"/new"}></Button> {/* Button */} 
                 </div>
-                <Hero h>={"88vh"}</Hero>
+
+                <Hero h={"88vh"}></Hero>  {/* Image  */}
+                
             </div>
         </React.Fragment>
-    )
+    );
 }
 
 export default Main
