12db3e97f4c001afd7f0a262cad7cf90bf00d0ad
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-05-26 21:14:07 -0600

    layout with import for new background

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import './Layout.css'
 import Header from "../../components/Header"
 import Footer from "../../components/Footer"
+import "../style/body.css"
 
 function Layout(props){
     return(
