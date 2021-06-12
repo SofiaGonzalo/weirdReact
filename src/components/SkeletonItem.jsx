b64295fd21a841a38eef3105f43a635d06464261
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-06-12 15:47:18 -0600

    skeleton item component

--- src/components/SkeletonItem.jsx
+++ src/components/SkeletonItem.jsx
@@ -1,13 +1,54 @@
-import React from "react"
-import Skeleton from "react-loading-skeleton"
-import "./style/SkeletonItem.css"
+import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "./style/BadgesList.css";
 
-const SkeletonItem = () =>{
-    return(
-        <React.Fragment>
-            <h1>Skeleton</h1>
-        </React.Fragment>
-    )
-}
+const SkeletonItem = () => {
+  return (
+    <React.Fragment>
+      {/* 1 */}
+      <div className="container List mb-5">
+        <div className="list-unstyled">
+          <div className="BadgesListItem mt-5">
+            <div className="col-5">
+              <div className="d-flex justify-content-center mt-1">
+                <Skeleton circle={true} height={100} width={100}></Skeleton>
+              </div>
+            </div>
+            <div className="col-7">
+              <div style={{ padding: "1em" }}>
+                <Skeleton count={3}></Skeleton>
+              </div>
+            </div>
+          </div>
+          <div className="BadgesListItem mt-5">
+            <div className="col-5">
+              <div className="d-flex justify-content-center mt-1">
+                <Skeleton circle={true} height={100} width={100}></Skeleton>
+              </div>
+            </div>
+            <div className="col-7">
+              <div style={{ padding: "1em" }}>
+                <Skeleton count={3}></Skeleton>
+              </div>
+            </div>
+          </div>
+          <div className="BadgesListItem mt-5">
+            <div className="col-5">
+              <div className="d-flex justify-content-center mt-1">
+                <Skeleton circle={true} height={100} width={100}></Skeleton>
+              </div>
+            </div>
+            <div className="col-7">
+              <div style={{ padding: "1em" }}>
+                <Skeleton count={3}></Skeleton>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-export default SkeletonItem
+    </React.Fragment>
+  );
+};
+
+export default SkeletonItem;
