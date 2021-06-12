ead97c0d720fcadaf70855cd24614b87db3c4e4a
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-06-12 17:10:14 -0600

    badges.jsx connected to badgeslist

--- src/pages/Badges/Badges.jsx
+++ src/pages/Badges/Badges.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Badges.css";
 import api from "../../libs/api";
+import BadgesList from "../../components/BadgesList";
 import SkeletonItem from "../../components/SkeletonItem";
 
 class Badges extends React.Component {
@@ -37,12 +38,13 @@ class Badges extends React.Component {
 
   render() {
     if (this.state.loading === true && !this.state.data) {
-      return <SkeletonItem></SkeletonItem>
+      return <SkeletonItem></SkeletonItem>;
     }
 
     return (
       <React.Fragment>
-        <h1>All badges</h1>
+        <div className="Badges__caontainer"></div>
+        <BadgesList badges={this.state.data}></BadgesList>
       </React.Fragment>
     );
   }
