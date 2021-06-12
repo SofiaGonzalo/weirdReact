0a20120a2d6ddd7afe11484668952a31ec452574
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-06-12 15:16:19 -0600

    skeletonitem component created with info

---
+++ src/components/SkeletonItem.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Skeleton from "react-loading-skeleton"
+import "./style/SkeletonItem.css"
+
+const SkeletonItem = () =>{
+    return(
+        <React.Fragment>
+            <h1>Skeleton</h1>
+        </React.Fragment>
+    )
+}
+
+export default SkeletonItem
