2539b6a31c3286e4fbe3894bff194eeea44f125c
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-06-12 15:48:20 -0600

    badges.jsx ready to work with skeletonitem

--- src/pages/Badges/Badges.jsx
+++ src/pages/Badges/Badges.jsx
@@ -1,40 +1,45 @@
 import React from "react";
 import "./Badges.css";
-import api from "../../libs/api"
+import api from "../../libs/api";
+import SkeletonItem from "../../components/SkeletonItem";
 
 class Badges extends React.Component {
-    
-    state={
-        loading: true,
-        error: null,
-        data: undefined
+  state = {
+    loading: true,
+    error: null,
+    data: undefined,
+  };
+
+  componentDidMount() {
+    this.fetchData();
+    this.setFetchInterval();
+  }
+
+  fetchData = async () => {
+    this.setState({ loading: true, error: null });
+    try {
+      const data = await api.badges.list();
+      data.reverse();
+      this.setState({ loading: false, data: data });
+    } catch (error) {
+      this.setState({ loading: false, error: error, data: [] });
+    }
+  };
+
+  setFetchInterval() {
+    //Updates data every 3 seconds
+    this.interval = setInterval(this.fetchData, 3000);
+  }
+
+  componentWillMount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    if (this.state.loading === true && !this.state.data) {
+      return <SkeletonItem></SkeletonItem>
     }
 
-    componentDidMount(){
-        this.fetchData();
-        this.setFetchInterval();
-    }
-
-    fetchData = async() =>{
-        this.setState({loading: true, error: null})
-        try{
-            const data = await api.badges.list();
-            data.reverse()
-            this.setState({loading: false, data: data})
-        }catch(error){
-            this.setState({loading:false, error: error, data: []})
-        }
-    }
-
-    setFetchInterval(){ //Updates data every 3 seconds
-        this.interval = setInterval(this.fetchData, 3000)
-    }
-
-    componentWillMount(){
-        clearInterval(this.interval)
-    }
- 
-    render() {
     return (
       <React.Fragment>
         <h1>All badges</h1>
