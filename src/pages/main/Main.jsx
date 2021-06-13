5060ea65088a1ebf897d76a8542c13d0a1c88db9
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-06-12 20:10:25 -0600

    footer changed and aded to every file instead of layout, working on main

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -2,22 +2,20 @@ import React from "react"
 import "./Main.css"
 import Hero from "../../components/Hero"
 import Button from "../../components/MainButton"
+import Footer from "../../components/Footer"
 
 const Main = (props) => {
     return(
         <React.Fragment>
             <div className="Main">
-
                 <div className="Main__content">
                     <h1 className="Main__title">{props.title || "Welcome"}</h1>
                     <Button contentText={"Don't click here"} Link={"/new"}></Button> {/* Button */} 
-
                     <Button contentText={"Definitely don't click here"} Link={"/log"}></Button> {/* Button */} 
                 </div>
-
                 <Hero h={"88vh"}></Hero>  {/* Image  */}
-                
             </div>
+            <Footer s={{bottom:0}}></Footer>
         </React.Fragment>
     );
 }
