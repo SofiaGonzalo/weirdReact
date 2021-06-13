6f99e08814f831887d065604eba37e5af30a3c4c
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-06-12 18:57:03 -0600

    erased footer from layout

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import './Layout.css'
 import Header from "../../components/Header"
-import Footer from "../../components/Footer"
+// import Footer from "../../components/Footer"
 import "../style/body.css"
 
 function Layout(props){
@@ -10,7 +10,6 @@ function Layout(props){
             <div className="Layout">
                 <Header></Header>
                 {props.children}
-                <Footer></Footer>
             </div>
         </React.Fragment>
     );
